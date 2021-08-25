283ea8ee4f5d759cbbdfe3f57a5f9ebf9aaf5eb5
unknown <Administrateur@139SE33-HXX04G2.ad.campus-eni.fr>
2021-08-25 14:15:59 +0200

    TP5 BucketList avec Doctrine

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,17 +14,33 @@ class WishController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function home(): Response
+    public function home(WishRepository $repo): Response
     {
-
+       $wishes = $repo->findBy(['isPublished'=> true],['dateCreated'=>'DESC']);
+      // $wishes = $repo->findAll(['dateCreated'=>'DESC']);
+        /*
         $wishes[] = 'Voyager en Corée du Sud';
         $wishes[] = 'Reprendre le sport';
         $wishes[] = 'Apprendre le japonais';
         $wishes[] = 'Arréter de fumer';
-        
+        */
         return $this->render('wish/home.html.twig', [
             'wishes' => $wishes
         ]);
+    }
+
+     /**
+     * @Route("/ajouter", name="ajouter")
+     */
+    public function ajouter(EntityManagerInterface  $em): Response
+    {
+        $wish = new Wish();
+        $wish->setTitle('Apprendre le Yoga');
+        $wish->setIsPublished(true);
+        $wish->setDateCreated(new \DateTime());
+        $em->persist($wish);
+        $em->flush();
+        return $this->redirectToRoute('home');
     }
 
     /**
